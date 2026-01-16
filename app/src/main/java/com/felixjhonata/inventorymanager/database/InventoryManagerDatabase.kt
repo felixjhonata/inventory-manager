1332562a77c24f55948a2cdd3178a6cc7c758de6
felixjhonata <felixjhonata3@gmail.com>
2026-01-16 23:47:12 +0700

    Revert breaking dependency update and disabled exportSchema for room db

--- app/src/main/java/com/felixjhonata/inventorymanager/database/InventoryManagerDatabase.kt
+++ app/src/main/java/com/felixjhonata/inventorymanager/database/InventoryManagerDatabase.kt
@@ -7,7 +7,7 @@ import com.felixjhonata.inventorymanager.database.dao.ProductDao
 import com.felixjhonata.inventorymanager.model.entity.Activity
 import com.felixjhonata.inventorymanager.model.entity.Product
 
-@Database(entities = [Product::class, Activity::class], version = 1)
+@Database(entities = [Product::class, Activity::class], version = 1, exportSchema = false)
 abstract class InventoryManagerDatabase: RoomDatabase() {
   abstract fun productDao(): ProductDao
   abstract fun activityDao(): ActivityDao
